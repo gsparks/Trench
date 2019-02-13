7cc93f8187e452a507876d03bd06646ca4463bc7
Greg Sparks <gsparks@westmont.edu>
2019-02-12 22:28:34 -0800

    Added gold class

--- items.py
+++ items.py
@@ -7,3 +7,10 @@ class Item():
 
     def __str__(self):
         return "{}/n=====\n{}\nValue: {}\n".format(self.name, self.description, self.value)"
+
+class Gold(Item):
+    def __init__(self, amt):
+        self.amt = amt
+        super().__init__(name = "Gold",
+                         description = "A round coin with {} stamped on the front.".format(str(self.amt)),
+                         value=self.amt)
