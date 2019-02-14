1da8a91165ed79b3c24d5232319658fd7e2b32cb
Greg Sparks <gsparks@westmont.edu>
2019-02-13 21:45:13 -0800

    Added Weapons

--- items.py
+++ items.py
@@ -14,3 +14,25 @@ class Gold(Item):
         super().__init__(name = "Gold",
                          description = "A round coin with {} stamped on the front.".format(str(self.amt)),
                          value=self.amt)
+
+class Weapon(Item):
+    def __init__(self, name, description, value, damage):
+        self.damage = damage
+        super().__init__(name, description, value)
+
+    def __str__(self):
+        return "{}\n=====\n{}\nValue: {}\nDamage: {}".format(self.name, self.description, self.value, self.damage)
+
+class Rock(Weapon):
+    def __init__(self):
+        super().__init__(name = "Rock",
+                         description = "A fist-sized rock, suitable for bludgeoning.",
+                         value = 0,
+                         damage = 5)
+
+class Dagger(Weapon):
+    def __init__(self):
+        super().__init__(name = "Dagger",
+                         description = "A small dagger with some rust. Somewhat more dangerous than a rock.",
+                         value = 10,
+                         damage = 10)
