412ad9b48a1842e3c5cc0e4ecc8c545d124db4aa
Greg Sparks <gsparks@westmont.edu>
2019-02-14 21:19:24 -0800

    Added world map

--- tiles.py
+++ tiles.py
@@ -42,4 +42,37 @@ class EnemyRoom(MapTile):
         if self.enemy.is_alive():
             the_player.hp = the_player.hp - self.enemy.damage
             print("Enemy does {} damage. You have {} HP remaining.".format(self.enemy.damage, the_player.hp))
-            
+
+class EmptyCavePath(MapTile):
+    def intro_text(self):
+        return """
+        Another unremarkable part of the cave. You must forge onwards.
+        """
+
+    def modify_player(self):
+        # Room has no action on player
+        pass
+
+class GiantSpiderRoom(EnemyRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, enemies.GiantSpider())
+
+    def intro_text(self):
+        if self.enemy.is_alive():
+            return """
+            Another unremarkable part of the cave. You must forge onwards.
+            """
+        else:
+            return """
+            The corpse of a dead spider rots on the ground.
+            """
+
+class FindDaggerRoom(LootRoom):
+    def __init__(self, x, y):
+        super().__init__(x, y, items.Dagger())
+
+    def intro_text(self):
+        return """
+        Your notice something shiny in the corner.
+        It's a dagger! You pick it up.
+        """
