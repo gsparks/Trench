13b2551a7616428a7d59f3621c2b059dbaa692da
Greg Sparks <gsparks@westmont.edu>
2019-02-19 21:27:35 -0800

    Added directions

--- player.py
+++ player.py
@@ -13,3 +13,20 @@ class Player():
     def print_inventory(self):
         for item in self.inventory:
             print(item, '\n')
+
+    def move(self, dx, dy):
+        self.location_x += dx
+        self.location_y += dy
+        print(world.tile_exists(self.location_x, self.location_y).intro_text())
+
+    def move_north(self):
+        self.move(dx=0, dy=-1)
+
+    def move_south(self):
+        self.move(dx=0, dy=1)
+
+    def move_east(self):
+        self.move(dx=1 dy=0)
+
+    def move_west(self):
+        self.move(dx=-1, dy=0)
