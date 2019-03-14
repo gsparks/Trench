9e318625ea34ae736c4ac50650fd996e9b95b249
Greg Sparks <gsparks@westmont.edu>
2019-03-13 22:26:37 -0700

    changed the staring text

--- tiles.py
+++ tiles.py
@@ -34,8 +34,11 @@ class MapTile:
 class StartingRoom(MapTile):
     def intro_text(self):
         return """
-        You find yourself if a cave with a flickering torch on the wall.
-        You can make out four paths, each equally as dark and foreboding.
+You manuver your submersible back into section 437 of the Trench. The navigation
+system failed some time ago and your only hope of reaching the surface is to
+follow the labyrinth of underwater veins back to the redevous point.
+
+You can make out four paths, each equally as dark and foreboding.
         """
 
     def modify_player(self, the_player):
